e30211525ca38e1d41410b4755168cb383226b69
Benjamin Wasilewski <benwasilewski@gmail.com>
2022-03-29 10:21:00 -0500

    remove unused imports

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import GetHeadlines from '../controllers/headlines'
 import styles from '../styles/Home.module.css'
 
 function Home({ headlines }) {
