02f9588f1e6417273be02c2e5efac98796c664f5
Benjamin Wasilewski <benwasilewski@gmail.com>
2022-03-29 10:20:49 -0500

    add goal graphic

--- components/footer.js
+++ components/footer.js
@@ -5,6 +5,7 @@ export default function Footer() {
 		<footer className={styles.footer}>
 			<div>
 				<a href="https://liberapay.com/bwasilewski/donate"><img alt="Donate using Liberapay" src="https://liberapay.com/assets/widgets/donate.svg" /></a>
+				<img src="https://img.shields.io/liberapay/goal/bwasilewski.svg?logo=liberapay"></img>
 			</div>
 			<div>
 				<small>&copy; 2022 Benjamin Wasilewski</small>
