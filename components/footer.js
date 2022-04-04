81c56e2c603cb7b3227d446a1d524b92cc891d6e
Benjamin Wasilewski <benwasilewski@gmail.com>
2022-04-04 00:26:46 -0500

    improve footer markup

--- components/footer.js
+++ components/footer.js
@@ -1,15 +1,19 @@
-import styles from '../styles/Home.module.css'
+import styles from '../styles/footer.module.css'
 
 export default function Footer() {
 	return (
 		<footer className={styles.footer}>
-			<div>
-				<a href="https://liberapay.com/bwasilewski/donate"><img alt="Donate using Liberapay" src="https://liberapay.com/assets/widgets/donate.svg" /></a>
-				<img src="https://img.shields.io/liberapay/goal/bwasilewski.svg?logo=liberapay"></img>
-			</div>
-			<div>
-				<small>&copy; 2022 Benjamin Wasilewski</small>
-			</div>
+			<ul>
+				<li>
+					<a href="https://liberapay.com/bwasilewski/donate"><img alt="Donate using Liberapay" src="https://liberapay.com/assets/widgets/donate.svg" /></a>
+				</li>
+				<li>
+					<img src="https://img.shields.io/liberapay/goal/bwasilewski.svg?logo=liberapay"></img>
+				</li>
+				<li>
+					<small>&copy; 2022 Benjamin Wasilewski</small>
+				</li>
+			</ul>
 		</footer>
 	)
 }
