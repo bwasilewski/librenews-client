7b03e69cca22a8cd895ef29f1549917265be97d0
Benjamin Wasilewski <benwasilewski@gmail.com>
2022-04-15 14:37:57 -0500

    set explicit width and height for donate image

--- components/footer.js
+++ components/footer.js
@@ -1,18 +1,23 @@
 import Image from 'next/image'
-import Signup from './signup'
 import styles from '../styles/Footer.module.css'
 
 export default function Footer() {
 	return (
 		<footer className={styles.footer}>
-			<Signup />
 			<ul>
 				<li>
-					<a href="https://liberapay.com/bwasilewski/donate"><Image alt="Donate using Liberapay" src="https://liberapay.com/assets/widgets/donate.svg" /></a>
+					<a href="https://liberapay.com/bwasilewski/donate">
+						<Image 
+							alt="Donate using Liberapay" 
+							src="https://liberapay.com/assets/widgets/donate.svg" 
+							width={81} 
+							height={30} 
+						/>
+					</a>
 				</li>
-				<li>
+				{/* <li>
 					<Image src="https://img.shields.io/liberapay/goal/bwasilewski.svg?logo=liberapay" />
-				</li>
+				</li> */}
 				<li>
 					<small>&copy; 2022 Benjamin Wasilewski</small>
 				</li>
