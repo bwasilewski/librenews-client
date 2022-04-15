cb6dbf538620b2dbf83f2805c476c6af566c1185
Benjamin Wasilewski <benwasilewski@gmail.com>
2022-04-15 14:38:14 -0500

    add liberapay.com to list of image sources

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true
+  trailingSlash: true,
+  images: {
+    domains: ['liberapay.com']
+  }
 }
 
 module.exports = nextConfig
