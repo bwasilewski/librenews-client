b66ccc91b0524aa2a4336cb7cbcb7b3e6925b7c2
Benjamin Wasilewski <benwasilewski@gmail.com>
2022-04-15 16:54:12 -0500

    simplify footer content

--- components/footer.js
+++ components/footer.js
@@ -1,27 +1,14 @@
-import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Footer.module.css'
 
 export default function Footer() {
 	return (
 		<footer className={styles.footer}>
-			<ul>
-				<li>
-					<a href="https://liberapay.com/bwasilewski/donate">
-						<Image 
-							alt="Donate using Liberapay" 
-							src="https://liberapay.com/assets/widgets/donate.svg" 
-							width={81} 
-							height={30} 
-						/>
-					</a>
-				</li>
-				{/* <li>
-					<Image src="https://img.shields.io/liberapay/goal/bwasilewski.svg?logo=liberapay" />
-				</li> */}
-				<li>
-					<small>&copy; 2022 Benjamin Wasilewski</small>
-				</li>
-			</ul>
+			<small>
+				<span>&copy; 2022</span>
+				<Link href="https://benwas.com">Benjamin Wasilewski</Link>
+				<span> | Contact Me</span>
+			</small>
 		</footer>
 	)
 }
