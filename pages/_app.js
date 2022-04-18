8d5ef8ad3968ad25c025d808b5f82b22ffdb71dd
Benjamin Wasilewski <benwasilewski@gmail.com>
2022-04-17 22:33:04 -0500

    add matomo tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,22 @@
 import Layout from '../components/layout'
+import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+
+  const inst = createInstance({
+    urlBase: 'https://stats.librenews.org',
+    siteId: 1,
+    disabled: false, // optional, false by default. Makes all tracking calls no-ops if set to true.
+    linkTracking: true, // optional, default value: true
+  })
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <MatomoProvider value={inst}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </MatomoProvider>
   )
 }
 
