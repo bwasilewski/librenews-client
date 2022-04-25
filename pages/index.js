e5b1a20c0e4f7e8106bb7959f67f3d4b1672af06
Benjamin Wasilewski <benwasilewski@gmail.com>
2022-04-25 00:16:01 -0500

    top stories component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react'
 import Head from 'next/head'
 import Headlines from '../components/headlines'
+import TopStories from '../components/topstories'
 import styles from '../styles/Home.module.css'
 import { useMatomo } from '@datapunt/matomo-tracker-react'
 
-function Home({ headlines, sources }) {
+function Home({ headlines, top }) {
   const { trackPageView } = useMatomo()
 
   useEffect(() => {
@@ -20,18 +21,29 @@ function Home({ headlines, sources }) {
       </Head>
 
       <main className={styles.main}>
-        { headlines && <Headlines headlines={headlines} /> }
+        <div className={styles.grid}>
+          <div className={styles.column_1}>
+            <Headlines headlines={headlines} />
+          </div>
+          <div className={styles.column_2}>
+            <h2>Top Stories</h2>
+            <TopStories stories={top} />
+          </div>
+        </div>
       </main>
     </div>
   )
 }
 
 Home.getInitialProps = async (ctx) => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/v1/headlines/fetch?size=20`)
+  const headlineResponse = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/v1/headlines/fetch?size=10`)
+  const topResponse = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/v1/headlines/top`)
 
   return { 
-    headlines: await response.json(),
+    headlines: await headlineResponse.json(),
+    top: await topResponse.json()
   }
 }
 
