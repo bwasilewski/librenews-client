5dc68668ab1ab40542f47d2ef6ab63cd056e7860
Benjamin Wasilewski <benwasilewski@gmail.com>
2022-04-29 11:01:37 -0500

    increase font size of footer content

--- components/footer.js
+++ components/footer.js
@@ -5,10 +5,12 @@ export default function Footer() {
 	const year = new Date().getFullYear()
 	return (
 		<footer className={styles.footer}>
-			<small>
-				&copy; {year}
-				<Link href="https://benwas.com">Benjamin Wasilewski</Link>
-			</small>
+			<p>
+				<b>
+					&copy; {year}
+					<Link href="https://benwas.com">Benjamin Wasilewski</Link>
+				</b>
+			</p>
 		</footer>
 	)
 }
