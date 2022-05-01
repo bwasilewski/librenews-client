776d3a509a57cafc54ec7b6081155813009180a4
Benjamin Wasilewski <benwasilewski@gmail.com>
2022-04-30 21:37:07 -0500

    remove headline click tracking

--- components/headlineitem.js
+++ components/headlineitem.js
@@ -1,9 +1,7 @@
 import Link from 'next/link'
 import PublishDate from './publishdate'
-import { useMatomo } from '@datapunt/matomo-tracker-react'
 
 export default function HeadlineItem ({headline, showCategory, showSource}) {
-	const { trackEvent } = useMatomo()
 	const {
 		category,
 		description,
@@ -13,15 +11,9 @@ export default function HeadlineItem ({headline, showCategory, showSource}) {
 		title,
 	} = headline
 
-	const handleClick = () => trackEvent({ 
-		category: 'headline', 
-		action: 'headline-click' 
-	})
-
 	return (
 		<li>
 			<a 
-				onClick={handleClick} 
 				title={description} 
 				href={link} rel="noreferrer" target="_blank">{title}
 			</a>
